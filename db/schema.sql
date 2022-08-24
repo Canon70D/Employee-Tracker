6004e74d5d0d5c06d9eff379bd74cdbe4657fcdd
FEIYU YIN <105698932+Canon70D@users.noreply.github.com>
2022-08-24 20:51:33 +0930

    created seeds.sql, minor update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE department (
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
+  title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,2) NOT NULL,
   department_id INT NOT NULL,
   FOREIGN KEY (department_id)
@@ -23,7 +23,7 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT NOT NULL,
+  manager_id INT,
   FOREIGN KEY (role_id)
   REFERENCES role(id)
   FOREIGN KEY (manager_id)
