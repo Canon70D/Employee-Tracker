315dee92007d42539ec10d30d12fca9db2015a4a
FEIYU YIN <105698932+Canon70D@users.noreply.github.com>
2022-08-25 14:24:16 +0930

    fix minor problem of db

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,13 @@ CREATE database company_db;
 USE company_db;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
 
 
-CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE roles (
+  id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,2) NOT NULL,
   department_id INT NOT NULL,
@@ -19,14 +19,14 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT,
   FOREIGN KEY (role_id)
-  REFERENCES role(id)
-  FOREIGN KEY (manager_id)
-  REFERENCES employee(id)
+  REFERENCES roles(id)
+  -- FOREIGN KEY (manager_id)
+  -- REFERENCES employee(id)
 );
 
